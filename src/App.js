e103025f3cd4c9bf99fad1d00b5f03cd5cdad97c
KalyanSreekar <kalyansreekarj@gmail.com>
2022-02-11 03:02:44 +0530

    basic images added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Drigger ka website.</p>
-      </header>
+      <img className="fullpageImage" src={require("./sanviinfra1.jpg")} />
+      <img className="fullpageImage" src={require("./sanviinfra2.jpg")} />
+      <img className="fullpageImage" src={require("./sanviinfra3.jpg")} />
+      <div className="ContactUsdiv">
+        <h1>Contact Us</h1>
+        <div className="ContactUsData">
+          <b className="data">sanviinfra@mapleheights.com</b>
+          <b className="data">+91 0123456789</b>
+        </div>
+      </div>
     </div>
   );
 }
